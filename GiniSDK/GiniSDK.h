0cc3aae0a4304a7c7fa4adee2b5de712e3f2eb96
Lukas Stührk <l.stuehrk@gini.net>
2014-05-27 14:30:23 +0200

    Initial commit.

---
+++ GiniSDK/GiniSDK.h
@@ -0,0 +1,13 @@
+//
+//  GiniSDK.h
+//  GiniSDK
+//
+//  Created by Lukas Stührk on 27/05/14.
+//  Copyright (c) 2014 Gini GmbH. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface GiniSDK : NSObject
+
+@end
---
+++ README.md
@@ -0,0 +1,6 @@
+# GINI SDK for iOS
+
+An SDK for integrating Gini into other apps.
+
+
+Copyright (c) 2014, Gini GmbH
