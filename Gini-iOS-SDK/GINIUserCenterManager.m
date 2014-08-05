221996133d1775bff600a1b78bbac6d7ce548062
Lukas Stührk <l.stuehrk@gini.net>
2014-08-05 14:35:53 +0200

    Implemented the GINIUserCenterManager.

---
+++ Gini-iOS-SDK/GINIUserCenterManager.m
@@ -0,0 +1,178 @@
+/*
+ *  Copyright (c) 2014, Gini GmbH.
+ *  All rights reserved.
+ */
+
+#import <Bolts/BFTask.h>
+#import "GINIUserCenterManager.h"
+#import "GINIURLSession.h"
+#import "GINISession.h"
+#import "GINIURLResponse.h"
+#import "GINISessionParser.h"
+#import "NSString+GINIAdditions.h"
+#import "GINIUser.h"
+
+
+@implementation GINIUserCenterManager {
+    id<GINIURLSession> _urlSession;
+    NSString *_clientID;
+    NSString *_clientSecret;
+    NSURL *_baseURL;
+
+    /**
+     * The active session to use for requests to the Gini User Center API.
+     *
+     * @warning These sessions have nothing to do with the sessions of the Gini API which are used in the
+     * `GINISessionManager`. Do not mix them up.
+     */
+    GINISession *_activeSession;
+}
+
++ (instancetype)userCenterManagerWithURLSession:(id <GINIURLSession>)urlSession clientID:(NSString *)clientID clientSecret:(NSString *)clientSecret baseURL:(NSURL *)baseURL {
+    return [[GINIUserCenterManager alloc] initWithURLSession:urlSession clientID:clientID clientSecret:clientSecret baseURL:baseURL];
+}
+
+- (instancetype)initWithURLSession:(id <GINIURLSession>)urlSession clientID:(NSString *)clientID clientSecret:(NSString *)clientSecret baseURL:(NSURL *)baseURL {
+    NSParameterAssert([urlSession conformsToProtocol:@protocol(GINIURLSession)]);
+    NSParameterAssert([clientID isKindOfClass:[NSString class]]);
+    NSParameterAssert([clientSecret isKindOfClass:[NSString class]]);
+    NSParameterAssert([baseURL isKindOfClass:[NSURL class]]);
+
+    if (self = [super init]) {
+        _urlSession = urlSession;
+        _clientID = clientID;
+        _clientSecret = clientSecret;
+        _baseURL = baseURL;
+    }
+    return self;
+}
+
+#pragma mark - Public methods
+- (BFTask *)getUserInfo:(NSString *)userID {
+    NSParameterAssert([userID isKindOfClass:[NSString class]]);
+
+    return [[self createMutableURLRequest:[NSString stringWithFormat:@"/api/users/%@", userID] httpMethod:@"GET"] continueWithSuccessBlock:^id(BFTask *requestTask) {
+        NSMutableURLRequest *urlRequest = requestTask.result;
+        [urlRequest setValue:@"application/json" forHTTPHeaderField:@"Accept"];
+        return [[_urlSession BFDataTaskWithRequest:urlRequest] continueWithSuccessBlock:^id(BFTask *task) {
+            GINIURLResponse *urlResponse = task.result;
+            return [GINIUser userFromAPIResponse:urlResponse.data];
+        }];
+    }];
+}
+
+- (BFTask *)createUserWithEmail:(NSString *)email password:(NSString *)password {
+    NSParameterAssert([email isKindOfClass:[NSString class]]);
+    NSParameterAssert([password isKindOfClass:[NSString class]]);
+
+    // This needs an active session with a bearer token.
+    return [[self createMutableURLRequest:@"/api/users" httpMethod:@"POST"] continueWithSuccessBlock:^id(BFTask *requestTask) {
+        NSMutableURLRequest *urlRequest = requestTask.result;
+        NSDictionary *payload = @{
+                @"email" : email,
+                @"password" : password
+        };
+        NSError *serializationError;
+        [urlRequest setHTTPBody:[NSJSONSerialization dataWithJSONObject:payload options:0 error:&serializationError]];
+        if (serializationError) {
+            return serializationError;
+        }
+        return [[_urlSession BFDataTaskWithRequest:urlRequest] continueWithSuccessBlock:^id(BFTask *createTask) {
+            GINIURLResponse *urlResponse = createTask.result;
+            NSString *location = [urlResponse.response.allHeaderFields valueForKey:@"Location"];
+            NSString *userId = [[location componentsSeparatedByString:@"/"] lastObject];
+            return [GINIUser userWithEmail:email userId:userId];
+        }];
+    }];
+}
+
+- (BFTask *)loginUser:(NSString *)userName password:(NSString *)password {
+    NSParameterAssert([userName isKindOfClass:[NSString class]]);
+    NSParameterAssert([password isKindOfClass:[NSString class]]);
+
+    // Login request (Please note that it uses the HTTP Basic authorization and not the Bearer token).
+    NSURL *URL = [NSURL URLWithString:@"/oauth/token?grant_type=password" relativeToURL:_baseURL];
+    NSMutableURLRequest *urlRequest = [NSMutableURLRequest requestWithURL:URL];
+    [urlRequest setHTTPMethod:@"POST"];
+    [urlRequest setValue:@"application/json" forHTTPHeaderField:@"Accept"];
+    [urlRequest setValue:[self createLoginHeader] forHTTPHeaderField:@"Authorization"];
+    // Login data (x-www-urlencoded).
+    NSData *loginData = [[NSString stringWithFormat:@"username=%@&password=%@", stringByEscapingString(userName), stringByEscapingString(password)] dataUsingEncoding:NSUTF8StringEncoding];
+    [urlRequest setHTTPBody:loginData];
+    [urlRequest setValue:@"application/x-www-urlencoded" forHTTPHeaderField:@"Content-Type"];
+
+    // Do the request and create the session form the response.
+    return [[_urlSession BFDataTaskWithRequest:urlRequest] continueWithSuccessBlock:^id(BFTask *loginTask) {
+        GINIURLResponse *response = loginTask.result;
+        return [GINISessionParser sessionWithJSONDictionary:response.data];
+    }];
+}
+
+#pragma mark - Private methods
+/**
+ * Gets a valid `GINISession` instance with a access token that could be used for requests to the Gini User Center API.
+ * If the app has not been logged in yet in this `GINIUserManager` instance or the access token has expired, the app is
+ * logged in again.
+ *
+ * @returns     A `BFTask*` that resolves to a `GINISession` instance.
+ */
+- (BFTask *)getSession {
+
+    // If there's already an active session, resolve to the session.
+    if (_activeSession && !_activeSession.hasAlreadyExpired) {
+        return [BFTask taskWithResult:_activeSession];
+    }
+    // Otherwise log in.
+    return [self login];
+}
+
+/**
+ * Logs-in the app so it gets an authentication token which can be used to do requests to the Gini User Center API.
+ *
+ * @returns     A `BFTask*` that resolves to a `GINISession` instance.
+ *
+ * @warning This login has nothing to do with the Gini API login. Please do not mix them up.
+ */
+- (BFTask *)login {
+
+    NSURL *loginURL = [NSURL URLWithString:@"/oauth/token?grant_type=client_credentials" relativeToURL:_baseURL];
+    NSMutableURLRequest *loginRequest = [NSMutableURLRequest requestWithURL:loginURL];
+    [loginRequest setHTTPMethod:@"GET"];
+    [loginRequest setValue:@"application/json" forHTTPHeaderField:@"Accept"];
+    [loginRequest setValue:[self createLoginHeader] forHTTPHeaderField:@"Authorization"];
+    return [[_urlSession BFDataTaskWithRequest:loginRequest] continueWithSuccessBlock:^id(BFTask *task) {
+        GINIURLResponse *response = task.result;
+        NSDictionary *sessionData = response.data;
+
+        GINISession *activeSession =  [GINISessionParser sessionWithJSONDictionary:sessionData];
+        _activeSession = activeSession;
+        return activeSession;
+    }];
+}
+
+/**
+ * Creates a new `NSString` that can be used as the HTTP Authorization header in a login request (HTTP Basic
+ * Authorization).
+ */
+- (NSString *)createLoginHeader {
+
+    // The HTTP basic authorization is a base64 encoded string with "username:password"
+    NSString *loginString = [NSString stringWithFormat:@"%@:%@", _clientID, _clientSecret];
+    NSData *loginData = [loginString dataUsingEncoding:NSUTF8StringEncoding];
+    NSData *base64LoginData = [loginData base64EncodedDataWithOptions:NSDataBase64Encoding64CharacterLineLength];
+    return [NSString stringWithFormat:@"Basic %@", [[NSString alloc] initWithData:base64LoginData encoding:NSUTF8StringEncoding]];
+}
+
+/**
+ * Creates a MutableURLRequest that has always a valid authentication token.
+ */
+- (BFTask *)createMutableURLRequest:(NSString *)URL httpMethod:(NSString *)httpMethod {
+    return [[self getSession] continueWithSuccessBlock:^id(BFTask *task) {
+        GINISession *session = task.result;
+        NSMutableURLRequest *urlRequest = [NSMutableURLRequest requestWithURL:[NSURL URLWithString:URL relativeToURL:_baseURL]];
+        [urlRequest setValue:[NSString stringWithFormat:@"BEARER %@", session.accessToken] forHTTPHeaderField:@"Authorization"];
+        [urlRequest setHTTPMethod:httpMethod];
+        return urlRequest;
+    }];
+}
+@end
---
+++ Gini-iOS-SDKTests/GINIUserCenterManagerSpec.m
@@ -0,0 +1,259 @@
+#import <Kiwi/Kiwi.h>
+#import <Bolts/BFTask.h>
+#import "GINIUserCenterManager.h"
+#import "GINIURLSession.h"
+#import "GINIURLSessionMock.h"
+#import "GINIURLResponse.h"
+#import "GINIUser.h"
+#import "GINISession.h"
+
+
+// Make the private methods of the GINIUserCenterManager visible in the test so it can be used in tests.
+@interface GINIUserCenterManager (TestVisibility)
+- (NSString *)createLoginHeader;
+@end
+
+SPEC_BEGIN(GINIUserCenterManagerSpec)
+
+    describe(@"The GINIUserCenterManager", ^{
+
+        /* The `GINIUserCenterManager` instance that is tested in the tests */
+        __block GINIUserCenterManager *userCenterManager;
+        /** The `GINIURLSession` instance used in the tests as the dependency of the userCenterManager. */
+        __block GINIURLSessionMock *urlSession;
+
+
+        /**
+         * Helper function which checks that the last request the GINIURLSessionMock received was to the correct URL and
+         * used the correct HTTP method.
+         */
+        __block void (^checkRequest)(NSString *URL, NSString *httpMethod) = ^(NSString *URL, NSString *httpMethod) {
+            // Check for the correct URL.
+            NSURLRequest *request = urlSession.lastRequest;
+            [[request shouldNot] beNil];
+            [[[[request URL] absoluteString] should] equal:URL];
+            [[request.HTTPMethod should] equal:httpMethod];
+        };
+
+        /**
+         * Helper function which checks that the last request the GINIURLSessionMock received had an authorization
+         * header with the correct bearer token (The token is from the fake server reply, see code below in the
+         * beforeEach block).
+         */
+        __block void (^checkAccessToken)() = ^() {
+            NSURLRequest *request = urlSession.lastRequest;
+            [[request shouldNot] beNil];
+            [[request.allHTTPHeaderFields[@"Authorization"] should] equal:@"BEARER 1234-5678"];
+        };
+
+        /**
+         * Helper function which checks that the last request the GINIURLSessionMock received had an authorization
+         * header with the correct HTTP basic authorization.
+         */
+        __block void (^checkBasicAuthentication)() = ^() {
+            NSURLRequest *request = urlSession.lastRequest;
+            [[request shouldNot] beNil];
+            [[request.allHTTPHeaderFields[@"Authorization"] should] equal:@"Basic Zm9vOmJhcg=="];
+        };
+
+        beforeEach(^{
+            urlSession = [GINIURLSessionMock new];
+            userCenterManager = [GINIUserCenterManager userCenterManagerWithURLSession:urlSession
+                                                                              clientID:@"foo"
+                                                                          clientSecret:@"bar"
+                                                                               baseURL:[NSURL URLWithString:@"https://user.gini.net/"]];
+
+            // Set the response for an authorization request, so the tests can receive an access token and can create
+            // a `GINISession` instance.
+            NSDictionary *sessionResponse = @{
+                @"access_token": @"1234-5678",
+                @"expires_in": @5000,
+                @"token_type": @"bearer"
+            };
+            [urlSession createAndSetResponse:sessionResponse forURL:@"https://user.gini.net/oauth/token?grant_type=client_credentials"];
+        });
+
+        it(@"should raise an error if instantiated with the wrong dependencies", ^{
+            [[theBlock(^{
+                [GINIUserCenterManager userCenterManagerWithURLSession:nil clientID:nil clientSecret:nil baseURL:nil];
+            }) should] raise];
+
+            [[theBlock(^{
+                [GINIUserCenterManager userCenterManagerWithURLSession:nil clientID:nil clientSecret:@"foo" baseURL:nil];
+            }) should] raise];
+
+            [[theBlock(^{
+                [GINIUserCenterManager userCenterManagerWithURLSession:nil clientID:@"foo" clientSecret:@"bar" baseURL:nil];
+            }) should] raise];
+        });
+
+        it(@"should build the correct login header", ^{
+            // Base64 of "foo:bar" (client ID : client secret)
+            [[[userCenterManager createLoginHeader] should] equal:@"Basic Zm9vOmJhcg=="];
+        });
+
+        context(@"the getUserInfo: method", ^{
+            it(@"should throw an error if getting the wrong arguments", ^{
+                [[theBlock(^{
+                    [userCenterManager getUserInfo:nil];
+                }) should] raise];
+            });
+
+            it(@"should do the HTTP request to the correct URL", ^{
+                [urlSession setResponse:[BFTask taskWithError:nil] forURL:@"https://user.gini.net/api/users/88a28076-18e8-4275-b39c-eaacc240d406"];
+                [userCenterManager getUserInfo:@"88a28076-18e8-4275-b39c-eaacc240d406"];
+                checkRequest(@"https://user.gini.net/api/users/88a28076-18e8-4275-b39c-eaacc240d406", @"GET");
+            });
+
+            it(@"should set the correct access token", ^{
+                [urlSession setResponse:[BFTask taskWithError:nil] forURL:@"https://user.gini.net/api/users/88a28076-18e8-4275-b39c-eaacc240d406"];
+                [userCenterManager getUserInfo:@"88a28076-18e8-4275-b39c-eaacc240d406"];
+                checkAccessToken();
+            });
+
+            it(@"should return a BFTask instance", ^{
+                [urlSession setResponse:[BFTask taskWithError:nil] forURL:@"https://user.gini.net/api/users/88a28076-18e8-4275-b39c-eaacc240d406"];
+                BFTask *infoTask = [userCenterManager getUserInfo:@"88a28076-18e8-4275-b39c-eaacc240d406"];
+                [[infoTask should] beKindOfClass:[BFTask class]];
+            });
+
+            it(@"should return a task that resolves to a GINIUser instance", ^{
+                NSURL *responseURL = [NSURL URLWithString:@"https://user.gini.net/api/users/88a28076-18e8-4275-b39c-eaacc240d406"];
+                NSHTTPURLResponse *response = [[NSHTTPURLResponse alloc] initWithURL:responseURL
+                                                                          statusCode:201
+                                                                         HTTPVersion:@"1.1"
+                                                                        headerFields:nil];
+                GINIURLResponse *giniResponse = [GINIURLResponse urlResponseWithResponse:response data:@{
+                    @"id":    @"88a28076-18e8-4275-b39c-eaacc240d406",
+                    @"email": @"foobar@example.com"
+                }];
+                [urlSession setResponse:[BFTask taskWithResult:giniResponse]
+                                 forURL:@"https://user.gini.net/api/users/88a28076-18e8-4275-b39c-eaacc240d406"];
+
+                BFTask *userTask = [userCenterManager getUserInfo:@"88a28076-18e8-4275-b39c-eaacc240d406"];
+                [[userTask.result should] beKindOfClass:[GINIUser class]];
+                GINIUser *user = userTask.result;
+                [[user.userId should] equal:@"88a28076-18e8-4275-b39c-eaacc240d406"];
+                [[user.userEmail should] equal:@"foobar@example.com"];
+            });
+        });
+
+        context(@"the createUserWithEmail:password: method", ^{
+            it(@"should throw an error if getting the wrong arguments", ^{
+                [[theBlock(^{
+                    [userCenterManager createUserWithEmail:nil password:nil];
+                }) should] raise];
+
+                [[theBlock(^{
+                    [userCenterManager createUserWithEmail:@"foobar" password:nil];
+                }) should] raise];
+
+            });
+
+            it(@"should do the HTTP request to the correct URL", ^{
+                [urlSession setResponse:[BFTask taskWithError:nil] forURL:@"https://user.gini.net/api/users"];
+                [userCenterManager createUserWithEmail:@"foobar@example.com" password:@"1234"];
+                checkRequest(@"https://user.gini.net/api/users", @"POST");
+            });
+
+            it(@"should set the correct access token", ^{
+                [urlSession setResponse:[BFTask taskWithError:nil] forURL:@"https://user.gini.net/api/users"];
+                [userCenterManager createUserWithEmail:@"foobar@example.com" password:@"1234"];
+                checkAccessToken();
+            });
+
+            it(@"should submit the correct data", ^{
+                [urlSession setResponse:[BFTask taskWithError:nil] forURL:@"https://user.gini.net/api/users"];
+                [userCenterManager createUserWithEmail:@"foobar@example.com" password:@"1234"];
+                NSURLRequest *lastRequest = urlSession.lastRequest;
+                NSString *expectedData = @"{\"password\":\"1234\",\"email\":\"foobar@example.com\"}";
+                NSString *actualData = [[NSString alloc] initWithData:lastRequest.HTTPBody encoding:NSUTF8StringEncoding];
+                [[expectedData should] equal:actualData];
+            });
+
+            it(@"should return a BFTask instance", ^{
+                [urlSession setResponse:[BFTask taskWithError:nil] forURL:@"https://user.gini.net/api/users"];
+                BFTask *createTask = [userCenterManager createUserWithEmail:@"foobar@example.com" password:@"1234"];
+                [[createTask should] beKindOfClass:[BFTask class]];
+            });
+
+            it(@"should return a task that resolves to a GINIUser instance", ^{
+                NSURL *responseURL = [NSURL URLWithString:@"https://user.gini.net/api/users"];
+                NSHTTPURLResponse *response = [[NSHTTPURLResponse alloc] initWithURL:responseURL
+                                                                          statusCode:201
+                                                                         HTTPVersion:@"1.1"
+                                                                        headerFields:@{
+                                                                            @"Location": @"https://user.gini.net/api/users/c1e60c6b-a0a4-4d80-81eb-c1c6de729a0e"
+                                                                        }];
+                GINIURLResponse *giniResponse = [GINIURLResponse urlResponseWithResponse:response];
+                [urlSession setResponse:[BFTask taskWithResult:giniResponse] forURL:@"https://user.gini.net/api/users"];
+
+                BFTask *createTask = [userCenterManager createUserWithEmail:@"foobar@example.com" password:@"1234"];
+                [[createTask.result should] beKindOfClass:[GINIUser class]];
+                GINIUser *user = createTask.result;
+                [[user.userId should] equal:@"c1e60c6b-a0a4-4d80-81eb-c1c6de729a0e"];
+                [[user.userEmail should] equal:@"foobar@example.com"];
+            });
+        });
+
+        context(@"the loginUser:password: method", ^{
+            it(@"should throw an error if getting the wrong arguments", ^{
+                [[theBlock(^{
+                    [userCenterManager createUserWithEmail:nil password:nil];
+                }) should] raise];
+
+                [[theBlock(^{
+                    [userCenterManager createUserWithEmail:@"foobar" password:nil];
+                }) should] raise];
+
+            });
+
+            it(@"should do the HTTP request to the correct URL", ^{
+                [urlSession setResponse:[BFTask taskWithError:nil] forURL:@"https://user.gini.net/oauth/token?grant_type=password"];
+                [userCenterManager loginUser:@"foobar@example.com" password:@"1234"];
+                checkRequest(@"https://user.gini.net/oauth/token?grant_type=password", @"POST");
+            });
+
+            it(@"should set the correct authentication headers", ^{
+                [urlSession setResponse:[BFTask taskWithError:nil] forURL:@"https://user.gini.net/oauth/token?grant_type=password"];
+                [userCenterManager loginUser:@"foobar@example.com" password:@"1234"];
+                checkBasicAuthentication();
+            });
+
+            it(@"should submit the correct data", ^{
+                [urlSession setResponse:[BFTask taskWithError:nil] forURL:@"https://user.gini.net/api/users"];
+                [userCenterManager loginUser:@"foobar@example.com" password:@"1234"];
+                NSURLRequest *lastRequest = urlSession.lastRequest;
+                NSString *expectedData = @"username=foobar%40example.com&password=1234";
+                NSString *actualData = [[NSString alloc] initWithData:lastRequest.HTTPBody encoding:NSUTF8StringEncoding];
+                [[expectedData should] equal:actualData];
+            });
+
+            it(@"should return a BFTask instance", ^{
+                [urlSession setResponse:[BFTask taskWithError:nil] forURL:@"https://user.gini.net/oauth/token?grant_type=password"];
+                BFTask *loginTask = [userCenterManager loginUser:@"foobar@example.com" password:@"1234"];
+                [[loginTask should] beKindOfClass:[BFTask class]];
+            });
+
+            it(@"should return a BFTask instance that resolves to a GINISession instance", ^{
+                NSURL *responseURL = [NSURL URLWithString:@"https://user.gini.net/oauth/token?grant_type=password"];
+                NSHTTPURLResponse *response = [[NSHTTPURLResponse alloc] initWithURL:responseURL
+                                                                          statusCode:201
+                                                                         HTTPVersion:@"1.1"
+                                                                        headerFields:nil];
+                GINIURLResponse *giniResponse = [GINIURLResponse urlResponseWithResponse:response data:@{
+                    @"access_token": @"6c470ffa-abf1-41aa-b866-cd3be0ee84f4",
+                    @"token_type":   @"bearer",
+                    @"expires_in":   @3599
+                }];
+                [urlSession setResponse:[BFTask taskWithResult:giniResponse] forURL:@"https://user.gini.net/oauth/token?grant_type=password"];
+
+                BFTask *loginTask = [userCenterManager loginUser:@"foobar@example.com" password:@"1234"];
+                [[loginTask.result should] beKindOfClass:[GINISession class]];
+                GINISession *session = loginTask.result;
+                [[session.accessToken should] equal:@"6c470ffa-abf1-41aa-b866-cd3be0ee84f4"];
+            });
+        });
+    });
+
+SPEC_END
