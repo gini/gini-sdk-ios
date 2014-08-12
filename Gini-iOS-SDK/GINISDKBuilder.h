e1f3250982d421156dd411d68c73bde2639ef395
Lukas Stührk <l.stuehrk@gini.net>
2014-08-12 17:36:05 +0200

    Added a builder for creating GiniSDK instances.

---
+++ Gini-iOS-SDK/GINISDKBuilder.h
@@ -0,0 +1,70 @@
+/*
+ *  Copyright (c) 2014, Gini GmbH.
+ *  All rights reserved.
+ */
+
+
+#import <Foundation/Foundation.h>
+
+@class GiniSDK;
+
+
+/**
+ * The GINISDKBuilder is used to create and configure new instances of the Gini SDK.
+ */
+@interface GINISDKBuilder : NSObject
+
+/**
+ * Creates an instance of the GINISDKBuilder where the client authorization flow is used.
+ *
+ * @param urlScheme         The custom URL scheme of the application that is used for the authorization flow. It is used
+ *                          when the browser redirects back to the app after a login. Must be the same as the custom URL
+ *                          you registered with Gini.
+ *
+ * @param clientID          The application's client ID for the Gini API.
+ */
++ (instancetype)clientFlowWithClientID:(NSString *)clientID urlScheme:(NSString *)urlScheme;
+
+/**
+ * Creates an instance of the GINISDKBuilder where the server authorization flow is used.
+ *
+ * @param urlScheme         The custom URL scheme of the application that is used for the authorization flow. It is used
+ *                          when the browser redirects back to the app after a login. Must be the same as the custom URL
+ *                          you registered with Gini.
+ *
+ * @param clientID          The application's client ID for the Gini API.
+ *
+ * @param clientSecret      The client secret you received from Gini.
+ */
+
++ (instancetype)serverFlowWithClientID:(NSString *)clientID clientSecret:(NSString *)clientSecret urlScheme:(NSString *)urlScheme;
+
+/**
+ * Creates an instance of the GINISDKBuilder where anonymous users are used.
+ *
+ * @param clientID          The application's clientID for the Gini API.
+ *
+ * @param userEmailDomain:  The domain of the email address.
+ *
+ * @warning: This requires access to the Gini User Center API. Access to the User Center API is restricted to selected
+ * clients only.
+ */
++ (instancetype)anonymousUserWithClientID:(NSString *)clientId clientSecret:(NSString *)clientSecret userEmailDomain:(NSString *)emaildomain;
+
+
+/**
+ * Use the sandbox environment (https://api-sandbox.gini.net/ and https://user-sandbox.gini.net/) instead of the
+ * production environment.
+ *
+ * This method returns the instance on which it is called, so it is possible to chain the configuration via builder
+ * methods.
+ */
+- (instancetype)useSandbox;
+
+
+/**
+ * Creates and returns the GiniSDK instance.
+ */
+- (GiniSDK *)build;
+
+@end
---
+++ Gini-iOS-SDK/GINISDKBuilder.m
@@ -0,0 +1,164 @@
+/*
+ *  Copyright (c) 2014, Gini GmbH.
+ *  All rights reserved.
+ */
+
+
+#import "GINISDKBuilder.h"
+#import "GiniSDK.h"
+#import "GINISessionManagerAnonymous.h"
+
+
+NSString *const GINIEmailDomainKey = @"emailDomain";
+
+
+GINIInjector* GINIDefaultInjector() {
+    GINIInjector *injector = [GINIInjector new];
+
+    // API Manager
+    [injector setObject:[NSURL URLWithString:@"https://api.gini.net/"] forKey:GINIInjectorAPIBaseURLKey];
+    [injector setSingletonFactory:@selector(apiManagerWithURLSession:requestFactory:baseURL:)
+                               on:[GINIAPIManager class]
+                           forKey:[GINIAPIManager class]
+                 withDependencies:@protocol(GINIURLSession), @protocol(GINIAPIManagerRequestFactory), GINIInjectorAPIBaseURLKey, nil];
+
+    // URLSession
+    [injector setFactory:@selector(urlSession)
+                      on:[GINIURLSession class]
+                  forKey:@protocol(GINIURLSession)
+        withDependencies:nil];
+
+    // APIRequestFactory
+    [injector setSingletonFactory:@selector(requestFactoryWithSessionManager:)
+                               on:[GINIAPIManagerRequestFactory class]
+                           forKey:@protocol(GINIAPIManagerRequestFactory)
+                 withDependencies:@protocol(GINISessionManager), nil];
+
+    // Session manager
+    [injector setObject:[NSURL URLWithString:@"https://user.gini.net/"] forKey:GINIInjectorUserBaseURLKey];
+    [injector setSingletonFactory:@selector(managerForClientFlowWithClientID:baseURL:URLSession:appURLScheme:)
+                               on:[GINISessionManager class]
+                           forKey:@protocol(GINISessionManager)
+                 withDependencies:GINIInjectorClientIDKey, GINIInjectorUserBaseURLKey, @protocol(GINIURLSession), GINIInjectorURLSchemeKey, nil];
+
+    // DocumentTaskManager
+    [injector setSingletonFactory:@selector(documentTaskManagerWithAPIManager:)
+                               on:[GINIDocumentTaskManager class]
+                           forKey:[GINIDocumentTaskManager class]
+                 withDependencies:[GINIAPIManager class], nil];
+
+    // Keychain manager
+    [injector setSingletonFactory:@selector(new)
+                               on:[GINIKeychainManager class]
+                           forKey:[GINIKeychainManager class]
+                 withDependencies:nil];
+
+    // Credentials store
+    [injector setSingletonFactory:@selector(credentialsStoreWithKeychainManager:)
+                               on:[GINIKeychainCredentialsStore class]
+                           forKey:@protocol(GINICredentialsStore)
+                 withDependencies:[GINIKeychainManager class], nil];
+
+    // User Center Manager
+    [injector setSingletonFactory:@selector(userCenterManagerWithURLSession:clientID:clientSecret:baseURL:)
+                               on:[GINIUserCenterManager class]
+                           forKey:[GINIUserCenterManager class]
+                 withDependencies:@protocol(GINIURLSession), GINIInjectorClientIDKey, GINIInjectorClientSecretKey, GINIInjectorUserBaseURLKey, nil];
+
+    return injector;
+}
+
+
+@implementation GINISDKBuilder {
+    GINIInjector *_injector;
+}
+
+#pragma mark - Factories
++ (instancetype)clientFlowWithClientID:(NSString *)clientID urlScheme:(NSString *)urlScheme {
+    NSParameterAssert([clientID isKindOfClass:[NSString class]]);
+    NSParameterAssert([urlScheme isKindOfClass:[NSString class]]);
+
+    return [[GINISDKBuilder alloc] initWithClientID:clientID urlScheme:urlScheme clientSecret:nil];
+}
+
++ (instancetype)serverFlowWithClientID:(NSString *)clientID clientSecret:(NSString *)clientSecret urlScheme:(NSString *)urlScheme {
+    NSParameterAssert([clientID isKindOfClass:[NSString class]]);
+    NSParameterAssert([clientSecret isKindOfClass:[NSString class]]);
+    NSParameterAssert([urlScheme isKindOfClass:[NSString class]]);
+
+    GINISDKBuilder *instance = [[GINISDKBuilder alloc] initWithClientID:clientID
+                                                              urlScheme:urlScheme
+                                                           clientSecret:clientSecret];
+    [instance useServerFlow];
+    return instance;
+}
+
++ (instancetype)anonymousUserWithClientID:(NSString *)clientId clientSecret:(NSString *)clientSecret userEmailDomain:(NSString *)emaildomain {
+    NSParameterAssert([clientId isKindOfClass:[NSString class]]);
+    NSParameterAssert([emaildomain isKindOfClass:[NSString class]]);
+    NSParameterAssert([clientSecret isKindOfClass:[NSString class]]);
+
+    GINISDKBuilder *instance = [[GINISDKBuilder alloc] initWithClientID:clientId urlScheme:nil clientSecret:clientSecret];
+    [instance useAnonymousUser:emaildomain];
+    return instance;
+}
+
+#pragma mark - Initializer
+- (instancetype)init{
+    @throw [NSException exceptionWithName:@"Not allowed"
+                                   reason:@"Use the designated initializer to initialize the SDK builder"
+                                 userInfo:nil];
+}
+
+- (instancetype)initWithClientID:(NSString *)clientID urlScheme:(NSString *)urlScheme clientSecret:(NSString *)clientSecret {
+    NSParameterAssert([clientID isKindOfClass:[NSString class]]);
+
+    if (self = [super init]) {
+        _injector = GINIDefaultInjector();
+        [_injector setObject:clientID forKey:GINIInjectorClientIDKey];
+        if (urlScheme != nil) {
+            [_injector setObject:urlScheme forKey:GINIInjectorURLSchemeKey];
+        }
+        if (clientSecret != nil) {
+            [_injector setObject:clientSecret forKey:GINIInjectorClientSecretKey];
+        }
+    }
+    return self;
+}
+
+
+#pragma mark - Public Methods
+- (instancetype)useSandbox {
+    [_injector setObject:[NSURL URLWithString:@"https://api-sandbox.gini.net/"] forKey:GINIInjectorAPIBaseURLKey];
+    [_injector setObject:[NSURL URLWithString:@"https://user-sandbox.gini.net/"] forKey:GINIInjectorUserBaseURLKey];
+    return self;
+}
+
+- (GiniSDK *)build {
+    return [[GiniSDK alloc] initWithInjector:_injector];
+}
+
+
+#pragma mark - Private configuration helpers
+- (void)useServerFlow {
+    // The default injector uses the client based authentication flow. We just set a new factory for the
+    // <GINISessionManager> protocol, so the server based authentication flow is used and we're done.
+    [_injector setSingletonFactory:@selector(managerForServerFlowWithClientID:clientSecret:credentialsStore:baseURL:URLSession:appURLScheme:)
+                                on:[GINISessionManager class]
+                            forKey:@protocol(GINISessionManager)
+                  withDependencies:GINIInjectorClientIDKey, GINIInjectorClientSecretKey, @protocol(GINICredentialsStore), GINIInjectorUserBaseURLKey, @protocol(GINIURLSession), GINIInjectorURLSchemeKey, nil];
+    [_injector setSingletonFactory:@selector(credentialsStoreWithKeychainManager:)
+                                on:[GINIKeychainCredentialsStore class]
+                            forKey:@protocol(GINICredentialsStore)
+                  withDependencies:[GINIKeychainManager class], nil];
+}
+
+- (void)useAnonymousUser:(NSString *)emailDomain {
+    [_injector setObject:emailDomain forKey:GINIEmailDomainKey];
+    [_injector setSingletonFactory:@selector(sessionManagerWithCredentialsStore:userCenterManager:emailDomain:)
+                                on:[GINISessionManagerAnonymous class]
+                            forKey:@protocol(GINISessionManager)
+                  withDependencies:@protocol(GINICredentialsStore), [GINIUserCenterManager class], GINIEmailDomainKey, nil];
+}
+
+@end
