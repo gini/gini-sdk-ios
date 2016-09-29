f0d5c70244af7833adcf2fc230c4e53e245bb93d
Peter Pult <p.pult@gini.net>
2016-09-29 14:51:48 +0200

    Fix virtualenv installation part 2

--- scripts/createIntegrationGuide.sh
+++ scripts/createIntegrationGuide.sh
@@ -18,7 +18,7 @@ cd $TRAVIS_BUILD_DIR/integration-guide
 cp -r $TRAVIS_BUILD_DIR/doc/* .
 
 curl -L -o virtualenv.py https://raw.github.com/pypa/virtualenv/master/virtualenv.py
-virtualenv ./virtualenv
+python virtualenv.py virtualenv
 source virtualenv/bin/activate
 pip install -r requirements.txt
 
