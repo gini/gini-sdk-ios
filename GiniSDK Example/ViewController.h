cd053a819fda5e04b5da29bc99558a740043a711
Peter Pult <p.pult@gini.net>
2016-10-13 17:46:36 +0200

    Add example app to enable keychain sharing in iOS 10

---
+++ GiniSDK Example/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GiniSDK Example
+//
+//  Created by Gini on 12/10/16.
+//  Copyright © 2016 Gini GmbH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ GiniSDK Example/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  GiniSDK Example
+//
+//  Created by Gini on 12/10/16.
+//  Copyright © 2016 Gini GmbH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
