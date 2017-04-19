2cda834007c0329dd314026e0e79fe3a125b9b68
Alpár Szotyori <a.szotyori@gini.net>
2017-04-19 14:45:45 +0200

    Fixed failing tests by always replacing shared application with our mock

--- Gini-iOS-SDKTests/GINISessionManagerSpecs.m
+++ Gini-iOS-SDKTests/GINISessionManagerSpecs.m
@@ -96,6 +96,7 @@ SPEC_BEGIN(GINISessionManagerSpecs)
             context(@"getSession", ^{
 
                 it(@"should return a task", ^{
+                    mockApplicationResponderForClientFlow(sessionManager, @"", @"", 0, NO);
                     [[[sessionManager getSession] should] beKindOfClass:[BFTask class]];
                 });
 
@@ -133,6 +134,7 @@ SPEC_BEGIN(GINISessionManagerSpecs)
             context(@"logIn", ^{
 
                 it(@"should return a task", ^{
+                    mockApplicationResponderForClientFlow(sessionManager, @"", @"", 0, NO);
                     [[[sessionManager logIn] should] beKindOfClass:[BFTask class]];
                 });
 
@@ -152,8 +154,7 @@ SPEC_BEGIN(GINISessionManagerSpecs)
                         return nil;
                     }];
 
-                    // Disabled due to error: [GINIUIApplicationMock beginBackgroundTaskWithName:expirationHandler:]: unrecognized selector sent to instance 0x600000226280
-                    // [[expectFutureValue(theValue([login isCompleted])) shouldAfterWaitOf(2)] beNo];
+                    [[expectFutureValue(theValue([login isCompleted])) shouldAfterWaitOf(2)] beNo];
                 });
             });
         });
@@ -200,6 +201,7 @@ SPEC_BEGIN(GINISessionManagerSpecs)
             context(@"getSession", ^{
 
                 it(@"should return a task", ^{
+                    mockApplicationResponderForServerFlow(sessionManager, @"", NO);
                     [[[sessionManager getSession] should] beKindOfClass:[BFTask class]];
                 });
 
@@ -256,6 +258,7 @@ SPEC_BEGIN(GINISessionManagerSpecs)
             context(@"logIn", ^{
 
                 it(@"should return a task", ^{
+                    mockApplicationResponderForServerFlow(sessionManager, @"", NO);
                     [[[sessionManager logIn] should] beKindOfClass:[BFTask class]];
                 });
 
@@ -272,8 +275,7 @@ SPEC_BEGIN(GINISessionManagerSpecs)
                         return nil;
                     }];
                     
-                    // Disabled due to error: [GINIUIApplicationMock beginBackgroundTaskWithName:expirationHandler:]: unrecognized selector sent to instance 0x600000226280
-                    //[[expectFutureValue(theValue([login isCompleted])) shouldAfterWaitOf(2)] beNo];
+                    [[expectFutureValue(theValue([login isCompleted])) shouldAfterWaitOf(2)] beNo];
                 });
             });
         });
