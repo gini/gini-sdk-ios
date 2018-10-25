59d8c418692177c4fa4bf7aed1ec713755f52679
Kike <enrique@gini.net>
2018-10-25 14:18:26 +0200

    Improved description for initializers

--- Gini-iOS-SDK/GINIDocumentMetadata.h
+++ Gini-iOS-SDK/GINIDocumentMetadata.h
@@ -13,21 +13,21 @@
     @property (readonly) NSDictionary<NSString *, NSString *> *headers;
 
 /**
- * The document metadata initializer.
+ * The document metadata initializer with only the branch ID (BLZ)
  *
  * @param branchId              The branch id - Bankleitzahl (BLZ)
  */
 - (instancetype) initWithBranchId:(NSString *)branchId;
 
 /**
- * The document metadata initializer.
+ * The document metadata initializer with only custom headers
  *
  * @param headers               Additional headers for the metadata. i.e: ["customerId":"123456"]
  */
 - (instancetype)initWithHeaders:(NSDictionary<NSString *,NSString *> *)headers;
 
 /**
- * The document metadata initializer.
+ * The document metadata initializer with the branch ID (BLZ) and additional custom headers
  *
  * @param branchId              The branch id - Bankleitzahl (BLZ)
  * @param additionalHeaders     Additional headers for the metadata. i.e: ["customerId":"123456"]
